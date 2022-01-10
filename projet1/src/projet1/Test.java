1698032fd23dc3830ef2030561ab3a55df96ddb8
MoodmaKing <yann.gueguen.dubois@gmail.com>
2022-01-10 01:04:59 +0100

    V1
    C'est ma v1

---
+++ projet1/src/projet1/Test.java
@@ -0,0 +1,11 @@
+package projet1;
+
+public class Test {
+
+	public static void main(String[] args){
+
+		System.out.println("hello");
+		System.out.println("hello v2");
+		System.out.println("hello v3");
+	}
+}
